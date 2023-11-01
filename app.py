8ba3537a34537b9f6241ef8d0673b44586b9e890
Karan Chrish <124337511+Karanchrish@users.noreply.github.com>
2023-11-01 12:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 import streamlit as st
 import numpy as np
-import joblib
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
 
-# Load the trained model
-model = joblib.load('movies.pkl')
+# Load the model
+model = keras.models.load_model('movies.h5')
 
-# Define a function to predict popularity
-def predict_popularity(vote_average, vote_count):
-    new_data = np.array([[vote_average, vote_count]])
-    scaled_new_data = scaler.transform(new_data)
-    predicted_popularity = model.predict(scaled_new_data)
-    return predicted_popularity[0][0]
+# Load the scaler
+scaler = StandardScaler()
+scaler.mean_ = np.array([7.0, 500])
+scaler.scale_ = np.array([1.0, 1.0])
 
-# Streamlit UI
-st.title('Movie Popularity Prediction')
+st.title('Movie Popularity Prediction App')
 
-# Input fields for user to enter vote_average and vote_count
-vote_average = st.slider('Vote Average', 0.0, 10.0, step=0.1)
-vote_count = st.number_input('Vote Count', min_value=0, max_value=10000, step=1)
+# Input for user to enter vote_average and vote_count
+vote_average = st.slider('Vote Average:', min_value=0.0, max_value=10.0, step=0.1, value=7.0)
+vote_count = st.slider('Vote Count:', min_value=0, max_value=10000, step=10, value=500)
 
-# Predict button
-if st.button('Predict Popularity'):
-    popularity_prediction = predict_popularity(vote_average, vote_count)
-    st.write(f'Predicted Popularity: {popularity_prediction:.2f}')
+# Transform the input data
+new_data = np.array([[vote_average, vote_count]])
+scaled_new_data = scaler.transform(new_data)
+
+# Predict the popularity
+predicted_popularity = model.predict(scaled_new_data)[0][0]
+
+# Display the prediction
+st.write("Predicted Popularity:", predicted_popularity)
