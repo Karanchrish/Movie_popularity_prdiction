e530ef5ce7c0092ff85ea803d128aa0aa1a7952b
Karan Chrish <124337511+Karanchrish@users.noreply.github.com>
2023-11-01 12:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ import joblib
 # Load the trained model
 model = joblib.load('movies.pkl')
 
-# Streamlit UI
-st.title("Movie Popularity Prediction")
+# Define a function to predict popularity
+def predict_popularity(vote_average, vote_count):
+    new_data = np.array([[vote_average, vote_count]])
+    scaled_new_data = scaler.transform(new_data)
+    predicted_popularity = model.predict(scaled_new_data)
+    return predicted_popularity[0][0]
 
-# Input for vote_average and vote_count
-vote_average = st.number_input("Vote Average:", min_value=0.0, max_value=10.0, step=0.1)
-vote_count = st.number_input("Vote Count:", min_value=0, step=1)
+# Streamlit UI
+st.title('Movie Popularity Prediction')
+
+# Input fields for user to enter vote_average and vote_count
+vote_average = st.slider('Vote Average', 0.0, 10.0, step=0.1)
+vote_count = st.number_input('Vote Count', min_value=0, max_value=10000, step=1)
 
 # Predict button
-if st.button("Predict Popularity"):
-    # Scale the input data
-    new_data = np.array([[vote_average, vote_count]])
-    predicted_popularity = model.predict(new_data)
-
-    st.write("Predicted Popularity:", predicted_popularity[0][0]/10000)
+if st.button('Predict Popularity'):
+    popularity_prediction = predict_popularity(vote_average, vote_count)
+    st.write(f'Predicted Popularity: {popularity_prediction:.2f}')
